7fc253dc8f665f7ee35c5dfe3043be32e205dd26
Alejandro Seguí <alesegdia@gmail.com>
2016-07-18 01:37:42 +0200

    planet prefab placeholder

---
+++ MiniLD69/Assets/Script/RandomPlanetShape.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomPlanetShape : MonoBehaviour {
+
+	public MeshFilter mesh;
+	Texture2D texture;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("meh");
+		texture = new Texture2D (100, 100);
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				texture.SetPixel (i, j, Color.red);
+			}
+		}
+		texture.Apply ();
+		mesh.GetComponent<MeshRenderer> ().material.SetTexture("_MainTex", texture);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
