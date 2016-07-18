86a577235e247010cb219fa7e688d7a541f9d482
Alejandro Seguí <alesegdia@gmail.com>
2016-07-19 01:56:57 +0200

    bootstraping PlanetSpawner; testing simple case

---
+++ MiniLD69/Assets/Script/PlanetSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetSpawner : MonoBehaviour {
+
+	public GameObject planetPrefab;
+
+	// Use this for initialization
+	void Start () {
+		SpawnPlanet ();
+	}
+
+	void SpawnPlanet( )
+	{
+		GameObject tr = GameObject.Instantiate(planetPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+		RandomPlanetShape rps = tr.gameObject.GetComponent<RandomPlanetShape>();
+		rps.BuildPlanetGraphics (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
