ae46179847cbac30a135f0b545719ae039199b31
Alejandro Seguí <alesegdia@gmail.com>
2016-07-19 12:13:04 +0200

    adding MapUtils

---
+++ MiniLD69/Assets/Script/MapUtils.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapUtils {
+
+	public static void CopyMatrixToTexture( float[][] map, Texture2D texture )
+	{
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				texture.SetPixel (i, j, new Color(map[i][j], map[i][j], map[i][j]));
+			}
+		}
+		texture.Apply ();
+	}
+
+}
