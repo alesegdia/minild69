8e2e980159dc0d6c05cb35fc09a065efe1b7692b
Alejandro Seguí <alesegdia@gmail.com>
2016-07-21 01:41:49 +0200

    using camera controller from Player

--- MiniLD69/Assets/Script/CameraController.cs
+++ MiniLD69/Assets/Script/CameraController.cs
@@ -9,20 +9,45 @@ public class CameraController : MonoBehaviour {
 	Vector3 objective;
 	float t = 0;
 
+	enum CamState {
+		Moving, Idle
+	};
+
+	CamState state = CamState.Idle;
+
+	OnReachDelegate reachDelegate;
+	Planet planet;
+
+	public delegate void OnReachDelegate( Planet planet );
+
+	public void GoToPlanet( Planet planet, OnReachDelegate reach_delegate )
+	{
+		this.planet = planet;
+		this.state = CamState.Moving;
+		this.objective = planet.transform.position;
+		this.objective.z = -2;
+		this.reachDelegate = reach_delegate;
+	}
+
 	// Use this for initialization
 	void Start () {
-		int selected = Random.Range (0, universe.planets.Count - 1);
-		objective = universe.planets[selected].transform.position;
-		objective.z = -2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (cam.transform.position != objective) {
-			t += Time.deltaTime / 10.0f;
-			cam.transform.position = Vector3.Lerp (cam.transform.position, objective, t);
-		} else {
-			t = 0;
+		switch (state) {
+		case CamState.Idle:
+			break;
+		case CamState.Moving:
+			if (cam.transform.position != objective) {
+				t += Time.deltaTime / 10.0f;
+				cam.transform.position = Vector3.Lerp (cam.transform.position, objective, t);
+			} else {
+				t = 0;
+				state = CamState.Idle;
+				reachDelegate (planet);
+			}
+			break;
 		}
 	}
 }
---
+++ MiniLD69/Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public UniverseGenerator universe;
+	public CameraController camController;
+
+	void ReachDelegate( Planet p )
+	{
+		Debug.Log ("ok im ere");
+	}
+
+	// Use this for initialization
+	void Start () {
+		int selected = Random.Range (0, universe.planets.Count - 1);
+		Planet p = universe.planets[selected].GetComponent<Planet>();
+		camController.GoToPlanet (p, ReachDelegate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
