809c1b4a48014ac35526a68898dbfed583a9f499
Alejandro Seguí <alesegdia@gmail.com>
2016-07-20 03:13:48 +0200

    adding properties to planet settings

--- MiniLD69/Assets/Script/PlanetSettings.cs
+++ MiniLD69/Assets/Script/PlanetSettings.cs
@@ -4,4 +4,6 @@ using System.Collections;
 public class PlanetSettings {
 	public Vector3 position { get; set; }
 	public int seed { get; set; }
+	public float temperature { get; set; }
+	public float size { get; set; }
 }
