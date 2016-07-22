9caaa05600d0eace004838075b1edfce33f510af
Alejandro Seguí <alesegdia@gmail.com>
2016-07-22 03:32:30 +0200

    making MapUtils function to copy matrix to texture generic

--- MiniLD69/Assets/Script/MapUtils.cs
+++ MiniLD69/Assets/Script/MapUtils.cs
@@ -5,8 +5,8 @@ public class MapUtils {
 
 	public static void CopyMatrixToTexture( float[][] map, Texture2D texture )
 	{
-		for (int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
+		for (int i = 0; i < map.Length; i++) {
+			for (int j = 0; j < map[0].Length; j++) {
 				texture.SetPixel (i, j, new Color(map[i][j], map[i][j], map[i][j]));
 			}
 		}
