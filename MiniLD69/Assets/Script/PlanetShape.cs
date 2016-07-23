9e5222f543fb0fc96f14d086753928c89befb19c
Alejandro Seguí <alesegdia@gmail.com>
2016-07-23 05:51:41 +0200

    working on planets graphics

--- MiniLD69/Assets/Script/PlanetShape.cs
+++ MiniLD69/Assets/Script/PlanetShape.cs
@@ -5,13 +5,13 @@ public class PlanetShape : MonoBehaviour {
 
 	MeshRenderer meshRenderer;
 	Texture2D texture;
-	int textureSize = 200;
+	int textureSize = 400;
 
 	// Use this for initialization
 	void Awake () {
 		meshRenderer = GetComponent<MeshRenderer> ();
 		texture = new Texture2D (textureSize, textureSize);
-		texture.filterMode = FilterMode.Point;
+		texture.filterMode = FilterMode.Bilinear;
 		meshRenderer.material.SetTexture("_MainTex", texture);
 	}
 
@@ -19,6 +19,11 @@ public class PlanetShape : MonoBehaviour {
 	{
 		float[][] map = NoiseUtils.GeneratePerlinNoise (textureSize, textureSize, 6, seed);
 		MapUtils.CopyMatrixToTexture (map, texture);
+		float r, g, b;
+		r = Random.value; g = Random.value; b = Random.value;
+		MapUtils.ColourGradient (map, texture,
+			new Color[] { new Color(r, g, b), new Color(3.0f * r/4.0f, 3.0f * g/4.0f, 3.0f * b/4.0f), new Color(2.0f * r/4.0f, 2.0f * g/4.0f, 2.0f * b/4.0f), new Color(r/4.0f, g/4.0f, b/4.0f) },
+			new float[] { 0.25f, 0.5f, 0.6f, 1.0f });
 	}
 	
 	// Update is called once per frame
