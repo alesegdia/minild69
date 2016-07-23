ae135ce9787f9f31d7e8f994671cefd2cab5fe52
Alejandro Seguí <alesegdia@gmail.com>
2016-07-23 05:51:07 +0200

    function to colour planets

--- MiniLD69/Assets/Script/MapUtils.cs
+++ MiniLD69/Assets/Script/MapUtils.cs
@@ -13,4 +13,21 @@ public class MapUtils {
 		texture.Apply ();
 	}
 
+	public static void ColourGradient( float[][] map, Texture2D texture, Color[] colors, float[] limits )
+	{
+		Debug.Assert( colors.Length == limits.Length );
+		for (int i = 0; i < map.Length; i++) {
+			for (int j = 0; j < map [0].Length; j++) {
+				for (int c = 0; c < colors.Length; c++) {
+					float value = map [i] [j];
+					if (value < limits [c]) {
+						texture.SetPixel (i, j, colors [c]);
+						break;
+					}
+				}
+			}
+		}
+		texture.Apply ();
+	}
+
 }
