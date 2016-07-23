8ca774b47dbfbcb75ac6a6314d20bcb943e8b456
Alejandro Seguí <alesegdia@gmail.com>
2016-07-23 05:52:01 +0200

    removing resource type from PlanetResourceProperties since it's known from array index

--- MiniLD69/Assets/Script/PlanetResourceProperties.cs
+++ MiniLD69/Assets/Script/PlanetResourceProperties.cs
@@ -6,5 +6,4 @@ using System.Collections;
 /// </summary>
 public class PlanetResourceProperties {
 	public float baseGatheringRate { get; set; }
-	public ResourceUtils.ResourceType resourceType { get; set; }
 }
