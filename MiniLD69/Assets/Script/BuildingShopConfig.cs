ebb56392baefeecd1c30b9034621e619cb0f5928
Alejandro Seguí <alesegdia@gmail.com>
2016-07-30 13:49:58 +0200

    working on shop

---
+++ MiniLD69/Assets/Script/BuildingShopConfig.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct BaseCosts {
+	public int froncetiteCost;
+	public int sandetiteCost;
+	public int xargonCost;
+}
+
+[System.Serializable]
+public struct BuildingShopEntryData {
+	[HeaderAttribute("Basic Info")]
+
+	public int ID;
+	public string name;
+	public BaseCosts baseCosts;
+
+	[HeaderAttribute("Player Info")]
+	public int currentUnitsInPlanet;
+
+	[TextAreaAttribute]
+	public string description;
+
+	[HeaderAttribute("Unlock Info")]
+	public int upgradeID;
+}
+
+public class BuildingShopConfig : MonoBehaviour {
+
+	public BuildingShopEntryData[] entries;
+	GameObject buildingShopEntryPrefab;
+	GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GameObject.Find ("/Canvas/Scroll View/Viewport/Content");
+		buildingShopEntryPrefab = Resources.Load ("ShopItemEntry") as GameObject;
+		int i = 0;
+		RectTransform rt = canvas.GetComponent<RectTransform> ();
+		rt.sizeDelta = new Vector2 (rt.sizeDelta.x, entries.Length * 310);
+		foreach( BuildingShopEntryData entry in entries )
+		{
+			GameObject go = GameObject.Instantiate (buildingShopEntryPrefab);
+			BuildingShopEntry goEntry = go.GetComponent<BuildingShopEntry>() as BuildingShopEntry;
+			goEntry.SetInfo (entry.name, entry.description);
+			goEntry.SetCosts (entry.baseCosts);
+			goEntry.transform.SetParent (canvas.transform);
+			goEntry.transform.localScale = new Vector3 (1, 1, 1);
+			go.transform.localPosition = new Vector3 (0, -160 - 310 * i, 0);
+			i++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MiniLD69/Assets/Script/BuildingShopEntry.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BuildingShopEntry : MonoBehaviour {
+
+	Text froncetiteCostText;
+	Text sandetiteCostText;
+	Text xargonCostText;
+	Text itemName;
+	Text itemDesc;
+
+	void Awake() {
+		froncetiteCostText = transform.Find ("FroncetiteCostIndicator/ResourceCost").gameObject.GetComponent<Text> ();
+		sandetiteCostText = transform.Find ("SandetiteCostIndicator/ResourceCost").gameObject.GetComponent<Text> ();
+		xargonCostText = transform.Find ("XargonCostIndicator/ResourceCost").gameObject.GetComponent<Text> ();
+		itemName = transform.Find ("ItemName_Text").gameObject.GetComponent<Text> ();
+		itemDesc = transform.Find ("ItemDescription_Text").gameObject.GetComponent<Text> ();
+	}
+
+	public void SetCosts(BaseCosts costs) {
+		froncetiteCostText.text = costs.froncetiteCost.ToString();
+		sandetiteCostText.text = costs.sandetiteCost.ToString();
+		xargonCostText.text = costs.xargonCost.ToString();
+	}
+
+	public void SetInfo( string name, string description ) {
+		itemName.text = name;
+		itemDesc.text = description;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
