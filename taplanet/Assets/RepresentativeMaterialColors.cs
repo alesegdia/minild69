31e8ec8accfedfb680c0d5a6101c75ba89f35141
Alejandro Seguí <alesegdia@gmail.com>
2016-11-14 03:47:40 +0100

    working on icons; modifying sandetite icon; adding representative material colors script

---
+++ taplanet/Assets/RepresentativeMaterialColors.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepresentativeMaterialColors : MonoBehaviour {
+
+	public Color froncetiteColor;
+	public Color sandetiteColor;
+	public Color xargonColor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
